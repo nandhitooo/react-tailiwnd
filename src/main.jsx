b0e768e2115047893d7af2708c57d2f2e7c966b4
Nandh1tooo <dhitopratama2005@gmail.com>
2026-01-30 19:36:35 +0700

    Fix import case for Navbar component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
 import Journey from "./components/Journey.jsx";
